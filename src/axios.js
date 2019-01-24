c9ca5a252124342a828d79e4ebd795da19766644
Yy <2956926710@qq.com>
2019-01-24 08:55:47 +0800

    完善登录注册

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? 'http://ele.kassing.cn' : 'www.xxx.com'
-
+axios.defaults.withCredentials=true;
 export default axios
